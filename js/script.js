21e3553a9a289727c63ed3fc668889c782355484
IvanShuklin <157677279+IvanShuklin@users.noreply.github.com>
2025-05-21 22:44:26 +0300

    Модальное окно

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 "use strict";
 
+const body = document.body;
 const modal = document.querySelector(".modal-container");
 const hotelSearchBtn = document.querySelector(".hotel-search-button");
 const modalCloseBtn = document.querySelector(".modal-close-button");
 
+function toggleModal(show) {
+  if (show) {
+    modal.classList.remove("is-hidden");
+    body.classList.add("modal-open"); //
+  } else {
+    modal.classList.add("is-hidden");
+    body.classList.remove("modal-open");
+  }
+}
+
 hotelSearchBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  modal.classList.remove("is-hidden");
+  toggleModal(true);
 });
 
 modalCloseBtn.addEventListener("click", function () {
-  modal.classList.add("is-hidden");
+  toggleModal(false);
 });
 
 modal.addEventListener("click", function (e) {
   if (e.target === modal) {
-    modal.classList.add("is-hidden");
+    toggleModal(false);
   }
 });
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("is-hidden")) {
-    modal.classList.add("is-hidden");
+    toggleModal(false);
   }
 });
